017fec4207d64cf65949dbfd4c4338c749695e6c
AfeefRazick <afeefrazickamir@gmail.com>
2023-08-31 21:47:48 +0530

    initial commit

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,21 +2,30 @@ import React from "react";
 import "./cell.css";
 
 class Celltemplate {
-  constructor(x, y, gridx, gridy) {
+  constructor(x, y, gridx, gridy, visited) {
+    this.ID = y + 1 + x * gridy;
+    this.visited = visited;
     this.X = x;
     this.Y = y;
 
-    this.top = y === 0 ? true : false;
-    this.bottom = y === gridy - 1 ? true : false;
-    this.left = x === 0 ? true : false;
-    this.right = x === gridx - 1 ? true : false;
+    this.neighbourIDs = [
+      y === 0 ? false : this.ID - 1, //top//
+      y === gridy - 1 ? false : this.ID + 1, //bottom//
+      x === 0 ? false : this.ID - gridy, //left//
+      x === gridx - 1 ? false : this.ID + gridy, //right//
+    ];
+
+    // (this.top = y === 0 ? false : this.ID - 1),
+    // (this.bottom = y === gridy - 1 ? false : this.ID + 1),
+    // (this.left = x === 0 ? false : this.ID - gridy),
+    // (this.right = x === gridx - 1 ? false : this.ID + gridy),
   }
 }
 export var cellsArray = {};
 
-export default function Cell({ Xpos, Ypos, gridx, gridy }) {
-  let newcell = new Celltemplate(Xpos, Ypos, gridx, gridy);
-  let newcellID = Ypos + 1 + Xpos * gridy;
+export default function Cell({ Xpos, Ypos, gridx, gridy, visited }) {
+  let newcell = new Celltemplate(Xpos, Ypos, gridx, gridy, visited);
+  let newcellID = newcell.ID;
   cellsArray[newcellID] = newcell;
-  return <div className="cell "></div>;
+  return <div className="cell" id={`cell-${newcellID}`}></div>;
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { React, useState } from "react";
 import Cell from "./Cell";
 import "./grid.css";
 import { v4 as uuidv4 } from "uuid";
 import { cellsArray } from "./Cell";
+
+const asyncdelay = async function (ms) {
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, ms)
+  );
+};
 
 const generateGrid = (gridX, gridY) => {
   const gridArray = [];
@@ -12,6 +20,47 @@ const generateGrid = (gridX, gridY) => {
     }
   }
   return gridArray;
+};
+
+const mazeGenerator = async () => {
+  // setcells(cellsArray);
+  const startnode = cellsArray[1];
+  console.log(startnode);
+  await generateMaze(startnode, 5, startnode);
+  console.log(cellsArray);
+};
+
+const generateMaze = async (prevnode, sec, prenode) => {
+  // setcells(cellsArray);
+  // await asyncdelay(100);
+  prevnode.visited = true;
+  console.log(prevnode);
+
+  let node = document.getElementById(`cell-${prevnode.ID}`);
+
+  if (prenode.ID - prevnode.ID == 1) {
+    node.style.borderTop = "white";
+  } else if (prenode.ID - prevnode.ID == -1) {
+    node.style.borderBottom = "white";
+  } else if (prenode.ID - prevnode.ID > 19) {
+    node.style.borderLeft = "white";
+  } else if (prenode.ID - prevnode.ID < -19) {
+    node.style.borderLeft = "white";
+  }
+  // node.style.backgroundColor = `rgb(${sec},128,128)`;
+
+  let neighbours = prevnode.neighbourIDs.filter(
+    (neighbour) => neighbour != false
+  );
+  neighbours = neighbours.sort(function () {
+    return Math.random() - 0.5;
+  });
+  // console.log(neighbours);
+  await neighbours.forEach(async (neighbou) => {
+    if (cellsArray[neighbou].visited === false) {
+      await generateMaze(cellsArray[neighbou], (sec * 2) % 250, prevnode);
+    }
+  });
 };
 
 export default function Grid({ Gridx, Gridy }) {
@@ -25,21 +74,28 @@ export default function Grid({ Gridx, Gridy }) {
     "--gridheight",
     `${cellwidth * Gridy + 2}px` //+2 is for border space//
   );
+
+  // const [cells, setcells] = useState(cellsArray);
+
   return (
     <div className="gridcontainer">
       <div className="grid">
         {generateGrid(Gridx, Gridy).map((cellCordinates) => {
           return (
             <Cell
-              key={uuidv4()}
+              key={cellCordinates[1] + 1 + cellCordinates[0] * Gridy}
               Xpos={cellCordinates[0]}
               Ypos={cellCordinates[1]}
               gridx={Gridx}
               gridy={Gridy}
+              visited={false}
             />
           );
         })}
       </div>
+      <button className="generatemaze" onClick={mazeGenerator}>
+        Generate Maze
+      </button>
       {/* {console.log(cellsArray)} */}
     </div>
   );
